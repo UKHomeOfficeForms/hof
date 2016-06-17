0747921d6a147caa26453948d2dcd891896c1270
JoeChapman <joseph.philip.chapman@gmail.com>
2016-06-17 17:12:41 +0100

    update session with config store

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const app = require('express')();
+const session = require('express-session');
 // const churchill = require('churchill');
 const hof = require('hof');
 const router = require('./lib/router');
@@ -73,7 +74,7 @@ module.exports = options => {
 
     statics(app, this.config);
     settings(app, this.config);
-    this.config.store(app, this.config);
+    sessions(app, session, this.config)
 
     load();
 
