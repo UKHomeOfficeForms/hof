219785d8165c2bd8da97288b0a3d14ddd9196182
Joe Chapman <joseph.philip.chapman@gmail.com>
2017-01-04 11:43:12 +0000

    Update fields for example (#97)
    - update helper to find fields from apps/${name}/fields.js if fields not set on route
    - amend readme for changes to fields
    - amend example, remove fields and view false override

--- example/index.js
+++ example/index.js
@@ -3,8 +3,6 @@
 const bootstrap = require('hof-bootstrap');
 
 bootstrap({
-  views: false,
-  fields: false,
   translations: './apps/example-app/translations',
   routes: [
     require('./apps/example-app')
