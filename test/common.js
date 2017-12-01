9dfd58ddb61c6ec83484a10483a10cd11e200f6a
Leonard Martin <leonard.martin@gmail.com>
2017-12-01 10:23:06 +0000

    Decouple custom csp directives and disabling csp (#160)
    If an app needs to set custom directives then it cannot currently also leave the disabling of csp by setting `DISABLE_CSP` env var intact, so implementations need to implement this as well as it being implemented in `hof`, which is not ideal.
    
    By splitting the disabling of csp from a top level `csp: false` property, to a sub-property of `disabled` then custom directives can be set, but will also be globally disabled independently, and the `DISABLE_CSP` environment variable is still respected.
    
    Leaves the option to disable by setting `csp: false` for backwards compatability.

--- test/common.js
+++ test/common.js
@@ -2,6 +2,7 @@
 
 global.chai = require('chai').use(require('sinon-chai'));
 global.should = chai.should();
+global.expect = chai.expect;
 global.sinon = require('sinon');
 require('sinomocha')();
 
