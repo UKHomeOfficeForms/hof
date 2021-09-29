3dd326715e5c33ac7a53c87cb7e6e82c56361c96
Alice Lui <alice.lui@digital.homeoffice.gov.uk>
2021-09-29 10:40:48 +0100

    address step created with validations

--- example/apps/example-app/fields.js
+++ example/apps/example-app/fields.js
@@ -9,5 +9,23 @@ module.exports = {
       className: 'visuallyhidden'
     },
     options: ['basic-form', 'complex-form', 'build-your-own-form']
+  },
+  // address steps from RIL 
+  building: {
+    validate: ['required', 'notUrl', { type: 'maxlength', arguments: 100 }]
+  },
+  street: {
+    validate: ['notUrl', { type: 'maxlength', arguments: 50 }],
+    labelClassName: 'visuallyhidden'
+  }, 
+  townOrCity: {
+    validate: ['required', 'notUrl',
+      { type: 'regex', arguments: /^([^0-9]*)$/ },
+      { type: 'maxlength', arguments: 100 }
+    ]
+  },
+  postcode: {
+    validate: ['required', 'postcode'],
+    formatter: ['removespaces', 'uppercase']
   }
 }
