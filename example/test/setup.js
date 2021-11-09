d2509e2191e04b73705e703a96862ec6d2b642f5
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2021-11-09 14:44:36 +0000

    remove unused code

--- example/test/setup.js
+++ example/test/setup.js
@@ -2,13 +2,11 @@
 
 'use strict';
 
-
 const chai = require('chai');
 
 process.env.NODE_ENV = 'test';
 
-global._ = require('lodash');
-global.chai = require('chai')
+global.chai = require('chai');
 global.should = chai.should();
 global.expect = chai.expect;
-
+global._ = require('lodash');
