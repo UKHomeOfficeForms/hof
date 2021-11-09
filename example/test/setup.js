e5c12966d5dd3373f042591fd12e9b07c816bb6a
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2021-11-09 12:19:30 +0000

    add unit test setup & test for summary data section

--- example/test/setup.js
+++ example/test/setup.js
@@ -2,12 +2,13 @@
 
 'use strict';
 
+
 const chai = require('chai');
 
 process.env.NODE_ENV = 'test';
 
-const chai = require('chai');
 global._ = require('lodash');
 global.chai = require('chai')
 global.should = chai.should();
 global.expect = chai.expect;
+
