23fad38d047287ec7533b2a4ea2aac389fbdeb5e
Alice Lui <alice.lui@digital.homeoffice.gov.uk>
2021-11-12 16:55:47 +0000

    Remove process.setMaxListeners from example app tests

--- example/test/setup.js
+++ example/test/setup.js
@@ -5,6 +5,3 @@ process.env.NODE_ENV = 'test';
 const chai = require('chai');
 global.chai = require('chai').use(require('sinon-chai'));
 global.should = chai.should();
-
-process.setMaxListeners(0);
-process.stdout.setMaxListeners(0);
