543817fa60163013c18692a3426011a13cd7c4d8
Alice Lui <alice.lui@digital.homeoffice.gov.uk>
2021-12-16 14:02:51 +0000

    Refactor the get-form-session behaviour

--- example/apps/example-app/behaviours/get-form-session.js
+++ example/apps/example-app/behaviours/get-form-session.js
@@ -5,7 +5,8 @@ const { response } = require('express');
 const { functions } = require('lodash');
 const baseUrl = 'http://localhost:3000/session'
 const moment = require('moment')
-const _ = require('lodash')
+const _ = require('lodash');
+const { error } = require('winston');
 
 module.exports = superclass => class extends superclass{
   locals(req, res) {
@@ -17,27 +18,37 @@ module.exports = superclass => class extends superclass{
     return locals;
   }
 
-  async getValues(req, res, next) {
-    await axios.get(baseUrl)
-    .then(function(response){
-      const resBody = response.data
-      if (resBody && resBody.length && resBody[0].session) {
-        req.sessionResults = [];
-        resBody.forEach(form => {
-        const created = moment(form.created_at);
-        const updated = moment(form.updated_at);  
-
-        const formSes = {
-          id: form.id,
-          session: JSON.stringify(form.session),
-          createdAt: created.format('DD MMMM YYYY'),
-          updatedAt: updated.format('DD MMMM YYYY')
-        }
-        req.sessionResults.push(formSes)
-      })
-      return req.sessionResults
+  getValues(req, res, next) {
+    super.getValues(req, res, err => {
+      if (err) {
+        next(err)
       }
+      axios.get(baseUrl)
+      .then(function(response){
+        const resBody = response.data
+        if (resBody && resBody.length && resBody[0].session){
+          req.sessionResults = [];
+          resBody.forEach(form => {
+            const created = moment(form.created_at)
+            const updated = moment(form.updated_at)
+
+            const formSession = {
+              id: form.id,
+              session: JSON.stringify(form.session),
+              createdAt: created.format('DD MMMM YYYY'),
+              updatedAt: updated.format('DD MMMM YYYY')
+            }
+            req.sessionResults.push(formSession)
+          })
+          console.log('session results', req.sessionResults)
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+      .then(function(){
+        next()
+      })     
     })
-    return super.getValues(req, res, next);
-  }
+  }  
 }
