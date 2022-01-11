535fe7b5b592ca5fece17dbbe60f627e9e8901f2
Robert McCann <robert.mccann@digital.homeoffice.gov.uk>
2022-01-11 16:17:32 +0000

    fix linting errors

--- test/example/behaviours/country-select.spec.js
+++ test/example/behaviours/country-select.spec.js
@@ -2,19 +2,16 @@
 
 const Behaviour = require('../../../example/apps/example-app/behaviours/country-select');
 const homeOfficeCountries = [''].concat(require('homeoffice-countries').allCountries);
-const Controller = require('../../../controller')
+const Controller = require('../../../controller');
 const _ = require('lodash');
 
-describe.only('apps/example-app/behaviours/country-select', () => {
-  let controller
+describe('apps/example-app/behaviours/country-select', () => {
+  let controller;
   let req;
   let res;
 
   beforeEach(() => {
     req = hof_request();
-
-    let sandbox;
-    sandbox = sinon.createSandbox();
 
     req.form.options = {fields: {
       countrySelect: {
@@ -23,19 +20,18 @@ describe.only('apps/example-app/behaviours/country-select', () => {
         }
       }
     }};
-    const CountrySelectController = Behaviour(Controller)
-    controller = new CountrySelectController({})
-
+    const CountrySelectController = Behaviour(Controller);
+    controller = new CountrySelectController({});
   });
 
   describe('countryselect', () => {
     it('checks to see if all country options labels contain country list in its fields', () => {
       controller.configure(req, res, () => {
-        const countryOptions = req.form.options.fields['countrySelect'].options;
+        const countryOptions = req.form.options.fields.countrySelect.options;
         const countryLabels = [''].concat(_.map(countryOptions, obj => obj.label));
-        countryLabels.concat(['Please select a country'])
-        homeOfficeCountries.splice(1 , 0, 'Please select a country')
-        countryLabels.should.deep.equal((homeOfficeCountries))
+        countryLabels.concat(['Please select a country']);
+        homeOfficeCountries.splice(1, 0, 'Please select a country');
+        countryLabels.should.deep.equal((homeOfficeCountries));
       });
     });
   });
