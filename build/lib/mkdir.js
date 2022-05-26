208debd172176a3b58a6274e0a7124f1c0a7cca6
Rhyad Zergane <rhyad.zergane@digital.homeoffice.gov.uk>
2022-05-26 14:17:10 +0100

    Removed fsPromises as node > 12.x doesn't support it and uses fs.promises but node < 11 doesn't support it

--- build/lib/mkdir.js
+++ build/lib/mkdir.js
@@ -1,6 +1,12 @@
 'use strict';
 
 const path = require('path');
-const fsPromises = require('fs/promises');
+const fs = require('fs');
 
-module.exports = file => fsPromises.mkdir(path.dirname(file), {recursive: true});
+// In node > 11 you can replace new promise with
+// fs.promises.mkdir(dir,opts,cb) or if > 10 and < 12
+// use fs/promises module but can't have both
+module.exports = file => new Promise((resolve, reject) => {
+  const dir = path.dirname(file);
+  fs.mkdir(dir, {recursive: true}, err => err ? reject(err) : resolve());
+});
