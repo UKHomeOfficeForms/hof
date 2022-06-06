1985177e49459ad88a1ee560a375626a3bb0d0f7
shamiluwais <mohamed.uwais@digital.homeoffice.gov.uk>
2022-06-06 14:40:54 +0100

    Pass host and port as parameters to redis createClient to resolve error

--- config/hof-defaults.js
+++ config/hof-defaults.js
@@ -20,7 +20,7 @@ const defaults = {
   host: process.env.HOST || '0.0.0.0',
   port: process.env.PORT || '8080',
   env: process.env.NODE_ENV || 'development',
-  gaTagId: process.env.GA_TAG,
+  gaTagId: process.env.GA_TAG || 'Test-GA-Tag',
   ga4TagId: process.env.GA_4_TAG,
   gaCrossDomainTrackingTagId: process.env.GDS_CROSS_DOMAIN_GA_TAG,
   loglevel: process.env.LOG_LEVEL || 'info',
