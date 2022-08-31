ac0eb8a7eec04d8b9af3e6903a59ddfe0c6e88fa
Rhyad Zergane <105197053+Rhyad-Zergane-HO@users.noreply.github.com>
2022-08-31 09:48:36 +0100

    Update UUID to v4 (#342)
    * Update UUID to v4
    * Update UUID to v4
    * To replace the deprecated
      UUID v1 package
    
    * Update package.json
    * Update version to beta.25
    * so that it can be published on
      npm

--- components/notify/notify.js
+++ components/notify/notify.js
@@ -1,10 +1,6 @@
 'use strict';
 const NotifyClient = require('notifications-node-client').NotifyClient;
-<<<<<<< HEAD
 const { v4: uuidv4 } = require('uuid');
-=======
-const uuid = require('uuid');
->>>>>>> 03704fb (add gov notify to the hof gov notify beta version)
 
 module.exports = class Notify {
   constructor(opts) {
@@ -15,11 +11,7 @@ module.exports = class Notify {
   }
 
   send(email) {
-<<<<<<< HEAD
     const reference = uuidv4();
-=======
-    const reference = uuid.v1();
->>>>>>> 03704fb (add gov notify to the hof gov notify beta version)
 
     return this.notifyClient.sendEmail(this.notifyTemplate, email.recipient, {
       personalisation: {
