879bde5706fbd9ad66c15d5fad0d09c2c4c7b8f1
Alexander Swann <alexander.swann@digital.homeoffice.gov.uk>
2022-10-07 17:00:31 +0100

    Add homeoffice-countries component for no-js nationality fields

---
+++ config/component-defaults.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  componentDefaults: {
+    homeOfficeCountries: [
+      'nationality',
+      'former-nationality',
+      'dual-nationality',
+      'nominated-nationality',
+      'nationality-error',
+      'country',
+      'someone-else-nationality'
+    ]
+  }
+};
