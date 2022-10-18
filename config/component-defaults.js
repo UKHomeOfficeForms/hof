a495411abffb4d0828802264c6de1d020a90fd09
Alexander Swann <alexander.swann@digital.homeoffice.gov.uk>
2022-10-18 12:40:43 +0100

    Fix components config

--- config/component-defaults.js
+++ config/component-defaults.js
@@ -1,15 +1,13 @@
 
 module.exports = {
-  componentDefaults: {
-    homeOfficeCountries: [
-      'nationality',
-      'former-nationality',
-      'dual-nationality',
-      'nominated-nationality',
-      'nationality-error',
-      'country',
-      'country-of-birth',
-      'someone-else-nationality'
-    ]
-  }
+  homeOfficeCountries: [
+    'nationality',
+    'former-nationality',
+    'dual-nationality',
+    'nominated-nationality',
+    'nationality-error',
+    'country',
+    'country-of-birth',
+    'someone-else-nationality'
+  ]
 };
