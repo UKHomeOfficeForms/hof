be0f61fbe8f2558b01ed0d7e579b9b07ff9289d5
andrew.gibson <andrew.gibson@digital.homeoffice.gov.uk>
2023-12-04 20:27:40 +0000

    Replace hardcoded app title

--- frontend/hods-template/build/config.js
+++ frontend/hods-template/build/config.js
@@ -1,24 +1,25 @@
 'use strict';
 /* eslint max-len: 0 */
 module.exports = {
-  htmlLang: '{{htmlLang}}',
-  assetPath: '{{govukAssetPath}}',
   afterHeader: '{{$afterHeader}}{{/afterHeader}}',
+  appTitle: '{{$journeyHeader}}{{/journeyHeader}}',
+  assetPath: '{{govukAssetPath}}',
   bodyClasses: '{{$bodyClasses}}{{/bodyClasses}}',
-  bodyStart: '{{$bodyStart}}{{/bodyStart}}',
   bodyEnd: '{{$bodyEnd}}{{/bodyEnd}}',
+  bodyStart: '{{$bodyStart}}{{/bodyStart}}',
   content: '{{$main}}{{/main}}',
   cookieMessage: '{{$cookieMessage}}{{/cookieMessage}}',
+  crownCopyrightMessage: '{{$crownCopyrightMessage}}© Crown copyright{{/crownCopyrightMessage}}',
   footerSupportLinks: '{{$footerSupportLinks}}{{/footerSupportLinks}}',
   footerTop: '{{$footerTop}}{{/footerTop}}',
+  globalHeaderText: '{{$globalHeaderText}}GOV.UK{{/globalHeaderText}}',
   head: '{{$head}}{{/head}}',
   headerClass: '{{$headerClass}}{{/headerClass}}',
   homepageUrl: '{{$homepageUrl}}https://www.gov.uk{{/homepageUrl}}',
-  logoLinkTitle: '{{$logoLinkTitle}}Go to the GOV.UK homepage{{/logoLinkTitle}}',
+  htmlLang: '{{htmlLang}}',
   insideHeader: '{{$insideHeader}}{{/insideHeader}}',
+  licenceMessage: '{{$licenceMessage}}All content is available under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" id="open-government-licence" class="govuk-footer__link" target="_blank" rel="license">Open Government Licence v3.0</a>, except where otherwise stated{{/licenceMessage}}',
+  logoLinkTitle: '{{$logoLinkTitle}}Go to the GOV.UK homepage{{/logoLinkTitle}}',
   pageTitle: '{{$pageTitle}}{{/pageTitle}}',
   propositionHeader: '{{$propositionHeader}}{{/propositionHeader}}',
-  globalHeaderText: '{{$globalHeaderText}}GOV.UK{{/globalHeaderText}}',
-  licenceMessage: '{{$licenceMessage}}All content is available under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" id="open-government-licence" class="govuk-footer__link" target="_blank" rel="license">Open Government Licence v3.0</a>, except where otherwise stated{{/licenceMessage}}',
-  crownCopyrightMessage: '{{$crownCopyrightMessage}}© Crown copyright{{/crownCopyrightMessage}}'
 };
