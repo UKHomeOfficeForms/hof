a316c52b624fc3a24c01efea76bb7306dbc79f6b
TemitopeAyokuHO <temitope.ayoku@digital.homeoffice.gov.uk>
2024-02-25 22:07:47 +0000

    HOFF-594 Create 500 error page (Internal Server Error)
    - Created a 500 error page in the view folder 500.html, this page render the content
    
    - Added few line to capture if status code is 500 (UNKNOWN or INTERNAL_SERVER_ERROR) in the Middleware/errors.js file
    
    - Added text in errors.json file which will be used to render error messages

--- middleware/errors.js
+++ middleware/errors.js
@@ -8,7 +8,6 @@ const errorMsg = code => `There is a ${code}_ERROR`;
 // eslint-disable-next-line complexity
 const getContent = (err, translate) => {
   const content = {};
-
   if (err.code === 'SESSION_TIMEOUT') {
     err.status = 401;
     err.template = 'session-timeout';
@@ -17,14 +16,12 @@ const getContent = (err, translate) => {
     content.title = (translate && translate('errors.session.title'));
     content.message = (translate && translate('errors.session.message'));
   }
-
   if (err.code === 'NO_COOKIES') {
     err.status = 403;
     err.template = 'cookie-error';
     content.title = (translate && translate('errors.cookies-required.title'));
     content.message = (translate && translate('errors.cookies-required.message'));
   }
-
   if (err.code === 'DDOS_RATE_LIMIT') {
     err.status = 429;
     err.template = 'rate-limit-error';
@@ -39,7 +36,6 @@ const getContent = (err, translate) => {
     content.timeToWait = rateLimitsConfig.rateLimits.requests.windowSizeInMinutes;
     content.postTimeToWait = (translate && translate('errors.ddos-rate-limit.post-time-to-wait'));
   }
-
   if (err.code === 'SUBMISSION_RATE_LIMIT') {
     err.status = 429;
     err.template = 'rate-limit-error';
@@ -54,10 +50,14 @@ const getContent = (err, translate) => {
     content.timeToWait = rateLimitsConfig.rateLimits.submissions.windowSizeInMinutes;
     content.postTimeToWait = (translate && translate('errors.submission-rate-limit.post-time-to-wait'));
   }
-
-  err.code = err.code || 'UNKNOWN';
-  err.status = err.status || 500;
-
+  if (err.code === 'UNKNOWN' || err.code === 'INTERNAL_SERVER_ERROR') {
+    err.status = 500;
+    err.template = '500';
+    err.title = (translate && translate('errors.500.title'));
+    err.message = (translate && translate('errors.500.description'));
+    content.title = (translate && translate('errors.500.title'));
+    content.message = (translate && translate('errors.500.description'));
+  }
   if (!content.title) {
     content.title = (translate && translate('errors.default.title')) || errorTitle(err.code);
   }
@@ -79,7 +79,6 @@ module.exports = options => {
   const opts = options || {};
   const logger = opts.logger;
   const debug = opts.debug;
-
   return (err, req, res, next) => {
     const translate = opts.translate || req.translate;
     const content = getContent(err, translate);
