7a6788bb3236d1bc01b8b0e63d30a60f7cb5479c
TemitopeAyokuHO <temitope.ayoku@digital.homeoffice.gov.uk>
2024-02-28 15:27:27 +0000

    moved all content to 404.html page

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -3,21 +3,11 @@
 const getTranslations = translate => {
   const translations = {
     title: 'Not found',
-    description: 'There is nothing here',
-    paragraph1: '',
-    paragraph2: '',
-    paragraph3: '',
-    paragraph3sub: '',
-    referencetag: ''
+    description: 'There is nothing here'
   };
   if (translate) {
     translations.title = translate('errors.404.title');
     translations.description = translate('errors.404.description');
-    translations.paragraph1 = translate('errors.404.paragraph1');
-    translations.paragraph2 = translate('errors.404.paragraph2');
-    translations.paragraph3 = translate('errors.404.paragraph3');
-    translations.paragraph3sub = translate('errors.404.paragraph3sub');
-    translations.referencetag = translate('errors.404.referencetag');
   }
   return translations;
 };
@@ -36,11 +26,6 @@ module.exports = options => {
     res.status(404).render('404', {
       title: translations.title,
       description: translations.description,
-      paragraph1: translations.paragraph1,
-      paragraph2: translations.paragraph2,
-      paragraph3: translations.paragraph3,
-      paragraph3sub: translations.paragraph3sub,
-      referencetag: translations.referencetag,
       // Finds the first word in the path of the
       // url and removes the leading slash.
       // Where url is `/foo/bar`, this returns `foo`.
