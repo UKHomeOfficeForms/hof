389064ce660d17be72aec8ce2d9143465ea91668
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2024-03-05 12:12:24 +0000

    HOFF-369: replace request with axios

--- controller/base-controller.js
+++ controller/base-controller.js
@@ -31,7 +31,6 @@ module.exports = class BaseController extends EventEmitter {
   requestHandler() {
     ['get', 'post', 'put', 'delete'].forEach(method => {
       if (typeof this[method] === 'function') {
-        // console.log('**********inside requestHandler************');
         this.router[method]('*', this[method].bind(this));
       } else {
         this.router[method]('*', (req, res, next) => {
--- test/functional-tests/lib/app.js
+++ test/functional-tests/lib/app.js
@@ -9,7 +9,6 @@ const partials = require('express-partial-templates');
 const template = require('../../../frontend').govUKTemplate;
 const cookieParser = require('cookie-parser');
 const mockPostcode = require('./mock-postcode');
-const helmet = require('helmet');
 
 const Wizard = require('../../../wizard');
 
@@ -19,7 +18,6 @@ module.exports = config => {
   app.set('view engine', 'html');
   app.engine('html', hogan);
   template({}, app);
-  app.use(helmet());
   app.use((req, res, next) => {
     req.translate = a => a;
     next();
@@ -27,13 +25,6 @@ module.exports = config => {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(cookieParser('not a secret'));
   app.use(session({ secret: 'not a secret', resave: true, saveUninitialized: false }));
-  // app.use((req, res, next) => {
-  //   helmet.contentSecurityPolicy({
-  //     directives: {
-  //       defaultSrc: ["'unsafe-eval'"]
-  //     }
-  //   })(req, res, next);
-  // });
   app.use(partials(app));
   app.use(mixins());
   app.use(Wizard(config.steps, config.fields, config.options));
