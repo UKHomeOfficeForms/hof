be6b2dab288ef51476d9a3403d6de57809b0a20b
Paola Maddaluno <83777310+PaolaDMadd-Pro@users.noreply.github.com>
2025-09-16 14:29:36 +0100

    refactor code to check id node_env is development (TBC)

--- build/tasks/browserify/compress.js
+++ build/tasks/browserify/compress.js
@@ -1,15 +1,15 @@
-'use strict';
+// 'use strict';
 
-const duplexify = require('duplexify');
-const spawn = require('child_process').spawn;
-const which = require('../../../lib/which');
-const bin = which('uglify-js', 'uglifyjs');
+// const duplexify = require('duplexify');
+// const spawn = require('child_process').spawn;
+// const which = require('../../../lib/which');
+// const bin = which('uglify-js', 'uglifyjs');
 
-const args = ['--comments', '/copyright|licen(c|s)e/i', '--'];
+// const args = ['--comments', '/copyright|licen(c|s)e/i', '--'];
 
-function uglify() {
-  const proc = spawn(bin, args, { stdio: 'pipe' });
-  return duplexify(proc.stdin, proc.stdout);
-}
+// function uglify() {
+//   const proc = spawn(bin, args, { stdio: 'pipe' });
+//   return duplexify(proc.stdin, proc.stdout);
+// }
 
-module.exports = uglify;
+// module.exports = uglify;
