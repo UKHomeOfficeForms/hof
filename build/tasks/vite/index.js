62eeeea26b288f223b31a78452aac84fbb9ddeed
Paola Maddaluno <83777310+PaolaDMadd-Pro@users.noreply.github.com>
2025-10-28 18:35:00 +0000

    index.js fixed

---
+++ build/tasks/vite/index.js
@@ -0,0 +1,21 @@
+const vite = require('vite');
+const path = require('path');
+const viteConfig = path.resolve(__dirname, './vite.config.js');
+
+module.exports = config => {
+// The Options.production’s value is set it up with a command flag
+//  in the package.json’s script. process.env.NODE_ENV);
+console.log('Vite build - production:', config.production);
+  if (config.production === true) {
+    return vite.build({
+      configFile: viteConfig
+    });
+  }
+  process.env.NODE_ENV = 'development'; // TO DO make this dynamic to Ensure
+  // the environment is set to development for non-production build
+  return vite.build({
+    configFile: viteConfig,
+    mode: 'development'
+  });
+};
+module.exports.task = 'vite';
