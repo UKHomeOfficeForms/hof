e734a8b0c07723e7845eff9e0b001ce9a104b5fa
Rhodine-orleans-lindsay <Rhodine.Orleans-Lindsay@digital.homeoffice.gov.uk>
2026-04-30 21:44:13 +0100

    HOFF-2085: Nunjucks Refactoring (bug fix) - Fix framework views like layout not being overridden by custom service level equivalent
    - refactor lib/settings.js so that custom service level views can override corresponding framework views e.g. layout.html
    - update how template-mixins.js and mixins test call nunjucks environment after changes to lib/settings
    - fix incorrect syntax in readme

--- frontend/template-mixins/mixins/template-mixins.js
+++ frontend/template-mixins/mixins/template-mixins.js
@@ -94,8 +94,7 @@ module.exports = function (options) {
 
     // use nunjucks environment for resolving includes/partials from the
     // project's views roots for this request.
-    const nunjucksEnv = (req && req.app && req.app.locals && req.app.locals.nunjucksEnv);
-
+    const nunjucksEnv = req?.res?.locals?.nunjucksEnv;
 
     // helper: resolve a template file path by trying express View, configured viewsDirectory and app roots
     function resolveTemplateFile(name) {
