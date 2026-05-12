5d592501855e356013fea73651ab585ecda86f0a
Paola Maddaluno <83777310+PaolaDMadd-Pro@users.noreply.github.com>
2026-05-12 19:48:14 +0100

    Hoff-2052 Nunjucks Refactoring(Address-Lookup)
    Update and refactored address-lookup component :
    address-lookup.html, address.html, postcode.html
    -renamed files extension to be compatible with Nunjucks
      * address-lookup.njk
      * address.njk
      * postcode.njk
    
    - Removed hogan from functional test library and from package .json
    - Update changelog.md
    - update functional tests to reflect the refactored component

--- test/functional-tests/lib/app.js
+++ test/functional-tests/lib/app.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const mixins = require('../../../frontend').mixins;
-const hogan = require('hogan-express-strict');
+const nunjucks = require('nunjucks');
 const bodyParser = require('body-parser');
 const partials = require('express-partial-templates');
 const template = require('../../../frontend').govUKTemplate;
@@ -14,10 +16,34 @@ const Wizard = require('../../../wizard');
 
 module.exports = config => {
   const app = express();
-  app.set('views', require('../../../frontend').partials.views);
-  app.set('view engine', 'html');
-  app.engine('html', hogan);
+  const views = require('../../../frontend').partials.views;
+  const govukRoot = path.dirname(require.resolve('govuk-frontend/package.json'));
+  const nunjucksEnv = new nunjucks.Environment(
+    new nunjucks.FileSystemLoader([
+      views,
+      govukRoot,
+      path.join(govukRoot, 'dist')
+    ]),
+    {
+      autoescape: true,
+      noCache: true
+    }
+  );
+  app.set('views', views);
+  app.set('view engine', 'njk');
+  app.engine('njk', (filePath, context, callback) => {
+    const env = context && context.nunjucksEnv ? context.nunjucksEnv : nunjucksEnv;
+    if (path.isAbsolute(filePath) && fs.existsSync(filePath)) {
+      const source = fs.readFileSync(filePath, 'utf8');
+      return env.renderString(source, context, callback);
+    }
+    return env.render(filePath, context, callback);
+  });
   template({}, app);
+  app.use((req, res, next) => {
+    res.locals.nunjucksEnv = nunjucksEnv;
+    next();
+  });
   app.use((req, res, next) => {
     req.translate = a => a;
     next();
