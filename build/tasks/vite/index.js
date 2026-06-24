ffeb3270a38be6b39c913f99152a463d27ffd574
Greg Austin <Gregory.Austin@digital.homeoffice.gov.uk>
2026-06-24 08:51:20 +0100

    Vite build task gets output location from config

--- build/tasks/vite/index.js
+++ build/tasks/vite/index.js
@@ -7,18 +7,23 @@ const hofDefaults = require('../../../config/hof-defaults');
 
 module.exports = config => {
   process.env.NODE_ENV = hofDefaults.env;
+  const publicDirectory = path.resolve(process.cwd(), config.js.outDir || 'public');
 
   if(!config.production) {
     return vite.build({
       configFile: viteConfig,
       mode: 'development',
       build: {
-        sourcemap: config.js.sourceMaps
+        sourcemap: config.js.sourceMaps,
+        outDir: publicDirectory
       }
     });
   }
   return vite.build({
-    configFile: viteConfig
+    configFile: viteConfig,
+    build: {
+        outDir: publicDirectory
+      }
   });
 };
 module.exports.task = 'vite';
