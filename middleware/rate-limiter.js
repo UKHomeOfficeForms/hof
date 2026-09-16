0eb23075d09a086e8855070e36e6840c27b73e78
dalglishdhandaHO <dalglish.dhanda1@digital.homeoffice.gov.uk>
2026-09-16 17:24:11 +0100

    HOFF-1999 revert and fix rate limiter

--- middleware/rate-limiter.js
+++ middleware/rate-limiter.js
@@ -74,9 +74,9 @@ module.exports = (options, rateLimitType) => {
         }
         // if number of requests made is less than allowed maximum, log new entry
         const lastRequestLog = data[data.length - 1];
-        const potentialCurrentWindowIntervalStartTimeStamp = currentRequestTime
-          .subtract(WINDOW_LOG_INTERVAL_IN_MINUTES, 'minutes')
-          .unix();
+        currentRequestTime = currentRequestTime
+          .subtract(WINDOW_LOG_INTERVAL_IN_MINUTES, 'minutes');
+        const potentialCurrentWindowIntervalStartTimeStamp = currentRequestTime.unix();
         //  if interval has not passed since last request log, increment counter
         if (lastRequestLog[timestampName] > potentialCurrentWindowIntervalStartTimeStamp) {
           lastRequestLog[countName]++;
